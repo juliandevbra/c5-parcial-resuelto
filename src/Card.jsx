bbbdaf2c6c03c61e2c4ac89aa44a54b0d34bbebb
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2023-06-09 21:23:48 -0300

    Parcial resuelto

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,12 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({user}) {
   return (
     <div>
-      <h2>Esto es un componente</h2>
+      <h2>Hola {user.name}</h2>
+      <h4>Tu color favorito es: </h4>
+      <h3>{user.color}</h3>
     </div>
   );
 }
